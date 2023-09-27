9854dc7599620270aebadb9952e6626dc096f6d1
feruzbekhamrayev8246 <101274511+feruzbekhamrayev8246@users.noreply.github.com>
2023-09-28 00:30:54 +0500

    Java spring boot bilan qilingan project maven texnologiyasi bilan PostgreSQL ga ulangan qismi

---
+++ src/main/java/com/example/pract1_pg/Pract1PgApplication.java
@@ -0,0 +1,13 @@
+package com.example.pract1_pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pract1PgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pract1PgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pract1_pg/Pract1PgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pract1_pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract1PgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
